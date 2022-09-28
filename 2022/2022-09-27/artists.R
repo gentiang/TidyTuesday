0c905a3736c0e4b3a3dcc757ccf7852fb6173ac2
gentiang <74708192+gentiang@users.noreply.github.com>
2022-09-28 22:35:10 +0200

    random tests

--- 2022/2022-09-27/artists.R
+++ 2022/2022-09-27/artists.R
@@ -6,6 +6,10 @@ library(tidyverse)
 library(ggplot2)
 library(janitor)
 library(scales)
+library(stringr)
 
-# Loading dataset
+# Loading datasets
 artists <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-09-27/artists.csv')
+
+
+
