d52f2c1c063da7c123ec1a96e1e3874fee3e2c0d
gentiang <74708192+gentiang@users.noreply.github.com>
2022-10-04 19:34:28 +0200

    initial commit and first lines of code

---
+++ 2022/2022-10-04/products.R
@@ -0,0 +1,26 @@
+# Setting work directory
+setwd("C:/Users/gashi/Desktop/TidyTuesday/2022/2022-10-04")
+
+# Loading Libraries
+library(tidytuesdayR)
+library(tidyverse)
+library(ggplot2)
+library(janitor)
+library(scales)
+library(stringr)
+library(ggbeeswarm)
+library(showtext)
+library(ggtext)
+library(glue)
+
+#Loading fonts
+font_add_google(name = "Montserrat", family = "Montserrat")
+font <- "Montserrat"
+
+# turn on showtext --------------------------------------------------------
+showtext_auto()
+
+# Loading datasets
+product_hunt <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-10-04/product_hunt.csv')
+
+# Data Processing
