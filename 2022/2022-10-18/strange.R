82b8e37ab480da37b38e9103006dd1ff738d8f24
gentiang <74708192+gentiang@users.noreply.github.com>
2022-10-23 22:22:14 +0200

    first commit

--- 2022/2022-10-18/strange.R
+++ 2022/2022-10-18/strange.R
@@ -10,7 +10,23 @@ library(showtext)
 library(ggtext)
 library(glue)
 library(lubridate)
+library(tidytext)
 
 
 
 episodes <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-10-18/episodes.csv')
+lines <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-10-18/stranger_things_all_dialogue.csv')
+
+dialogue_clean <- lines |>
+  select(dialogue, season, episode, line) |>
+  unnest_tokens(word, dialogue) |>
+  anti_join(get_stopwords(), by = "word") |>
+  na.omit()
+
+
+stranger_things_dialogue <- lines %>%
+  unnest_tokens(word, dialogue) %>%
+  filter(!is.na(word)) %>%
+  anti_join(stop_words, by = "word") %>%
+  count(season, episode, start_time, word) %>%
+  mutate(start_time = lubridate::minute(start_time))
